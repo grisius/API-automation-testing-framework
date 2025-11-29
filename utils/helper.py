1477018f940bbd63247f2f1f1c3c6847a88f2e24
smoke-story <gray0991@gmail.com>
2025-11-29 20:43:59 +0300

    helper.py was updated..

--- utils/helper.py
+++ utils/helper.py
@@ -2,8 +2,9 @@ import json
 import allure
 from allure_commons.types import AttachmentType as Atype
 
+
 class Helper:
 
-    def attach_response(self, response):
+    def attach_response(self, response) -> None:
         response = json.dumps(response, indent=4)
         allure.attach(body=response, name="API Response", attachment_type=Atype.JSON)
