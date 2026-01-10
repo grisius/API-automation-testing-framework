a6343bc94a59b8c1662410e6540c3b79454335e3
smoke-story <gray0991@gmail.com>
2026-01-10 16:28:27 +0300

    tests updated..

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,8 +1,14 @@
+import pytest
+
 from config.base_test import BaseTest
+import allure
 
 
+@allure.feature("Pet")
 class TestPet(BaseTest):
 
+    @pytest.mark.regression
+    @allure.title("Add new pet to store")
     def test_add_new_pet_to_store(self):
         pet = self.api_pet.add_new_pet_to_store()
         print(pet.name, pet.category.id, pet.category.name, sep="\n")
